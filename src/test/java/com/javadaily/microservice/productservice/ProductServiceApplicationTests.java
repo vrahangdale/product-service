c6c91d6bbf2672df8bfe4f83fcd274f5b4f4edaa
Vaibhav Rahangdale <vrahangdale@unomaha.edu>
2021-02-06 17:36:09 -0800

    initial commit

---
+++ src/test/java/com/javadaily/microservice/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadaily.microservice.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
